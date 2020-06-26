6824f2801fa1321dd85be736ccc843a91884a9eb
Mike Shemanskyi <mike@m11-mac-mini.local>
2020-06-26 20:15:42 +0300

    fix

--- src/main/java/com/m11/util/OSCheck.java
+++ src/main/java/com/m11/util/OSCheck.java
@@ -3,20 +3,18 @@ package com.m11.util;
 public class OSCheck {
     static String OS = System.getProperty("os.name").toLowerCase();
     public static boolean isWindows() {
-
         return (OS.indexOf("win") >= 0);
-
     }
 
     public static boolean isMac() {
-
         return (OS.indexOf("mac") >= 0);
-
     }
 
     public static boolean isUnix() {
-
-        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
-
+        return (OS.indexOf("nix") >= 0
+                || OS.indexOf("nux") >= 0
+                || OS.indexOf("aix") > 0
+                || OS.indexOf("linux") > 0
+        );
     }
 }
